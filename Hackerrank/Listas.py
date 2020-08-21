78da7905c9cad027180eba085b7e692a2561ea45
Cacatuaa <nicolascbrisque@gmail.com>
2020-08-21 18:42:48 -0300

    teste 2

--- Hackerrank/Listas.py
+++ Hackerrank/Listas.py
@@ -1,4 +1,5 @@
 """
+Teste Git
 Consider a list (list = []). You can perform the following commands:
 
 insert i e: Insert integer  at position .
@@ -9,7 +10,6 @@ sort: Sort the list.
 pop: Pop the last element from the list.
 reverse: Reverse the list.
 Initialize your list and read in the value of  followed by  lines of commands where each command will be of the  types listed above. Iterate through each command in order and perform the corresponding operation on your list.
-teste
 """
 def list_insert(lista, index, number):
     lista.insert(index, number)
