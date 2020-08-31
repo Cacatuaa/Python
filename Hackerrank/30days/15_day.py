8f61312d62520fdb45dc46f327030127ae818970
Cacatuaa <nicolascbrisque@gmail.com>
2020-08-31 19:46:22 -0300

    Day 15 of Hackerrank - 30 Days Challenge

---
+++ Hackerrank/30days/15_day.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        temp = Node(data)
+        if head is None:
+            head = temp
+            return head
+
+        current = head
+
+        while current.next is not None:
+            current = current.next
+        current.next = temp
+        
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head)	  
