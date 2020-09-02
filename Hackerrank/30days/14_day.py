38dc0b3f655df604801726c160114ab447ae63e6
Cacatuaa <nicolascbrisque@gmail.com>
2020-09-02 12:12:20 -0300

    Day 14 of Hackerrank - 30 Days Challenge

---
+++ Hackerrank/30days/14_day.py
@@ -0,0 +1,18 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+        self.maximumDifference = 0
+
+	# Add your code here
+    def computeDifference(self):
+        array = sorted(self.__elements)
+        self.maximumDifference = abs(array[0] - array[len(array)-1])
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
